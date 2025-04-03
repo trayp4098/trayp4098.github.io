8927a33e9992a6fe0fba7fff6ccf043afbec0fd9
траур <vovchik200679@gmail.com>
2025-04-03 14:20:27 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -94,15 +94,52 @@ var app = new Vue({
                 }
             }
         },
-        checkInCart: function(){
-            // Placeholder for checking if the current product is in the cart
-            // This would likely involve iterating through the 'cart' array
-            console.log("Checking if product is in cart");
+        addToCart:function(id){
+            if(window.localStorage.getItem('cart')){
+                this.cart=window.localStorage.getItem('cart').split(',');
+            }
+
+            if(this.cart.indexOf(String(id))==-1){
+                this.cart.push(id);
+                window.localStorage.setItem('cart',this.cart.join());
+                this.btnVisible=1;
+            }
         },
-        getCart: function(){
-            // Placeholder for fetching the cart data
-            // This might involve an API call or retrieving data from local storage
-            console.log("Fetching cart data");
+        checkInCart:function(){
+            if(this.product && this.product.id && window.localStorage.getItem('cart').split(',').indexOf(String(this.product.id))!=-1) this.btnVisible=1;
+            if (window.localStorage.getItem('cart') !== null) this.cartVisible = 1;
+        },
+        getCart:function(){
+            if(window.localStorage.getItem('cart')){
+                this.cart=window.localStorage.getItem('cart').split(',');
+                for(var value of this.cart){
+                    for(var index in this.products){
+                        if(value == this.products[index].id ){
+                            this.product.push(this.products[index])
+                        }
+                    }
+                }
+            }
+        },
+        removeFromCart:function(id){
+            for(var index in this.product){
+                if(id ==  this.product[index].id){
+                    this.product.splice(index,1);
+                    this.cart.splice(index,1)
+                }
+            }
+            window.localStorage.setItem('cart', this.cart.join(','));
+            this.getCart();
+            location.reload();
+        },
+        makeOrder:function(){
+            
+            this.formVisible=0;
+            this.cartVisible=0;
+            
+            this.cart = [];
+            window.localStorage.removeItem('cart');
+            alert("Ваш запит надіслано. Натисніть OK, щоб оновити сторінку.");
         }
-    }
+    },
 });
